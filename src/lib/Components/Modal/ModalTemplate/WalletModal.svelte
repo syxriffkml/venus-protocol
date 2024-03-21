4e5d2a9b697e3604b4e31d502f176eab9ebf89c6
Strawberry Cheesecake <55937579+syxriffkml@users.noreply.github.com>
2024-03-21 14:37:56 +0800

    added onclick

--- src/lib/Components/Modal/ModalTemplate/WalletModal.svelte
+++ src/lib/Components/Modal/ModalTemplate/WalletModal.svelte
@@ -21,7 +21,7 @@
 
 <div class="grid grid-cols-3 sm:grid-cols-4 mx-auto my-2 justify-items-center space-x-1 space-y-1">
     {#each wallet as select_wallet}
-        <button class="w-[103px] h-[90px] rounded-xl hover:bg-[#2d3549] flex flex-col items-center justify-center gap-y-2">
+        <button class="w-[103px] h-[90px] rounded-xl hover:bg-[#2d3549] flex flex-col items-center justify-center gap-y-2" on:click={() => { console.log(select_wallet.name) }}>
             {@html select_wallet.icon}
             <p class="text-xs whitespace-nowrap">{select_wallet.name}</p>
         </button>
