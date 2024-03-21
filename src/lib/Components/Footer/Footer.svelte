c7d8b3a2f22ad87cf8654e35dc6ecd41409f9f97
Strawberry Cheesecake <55937579+syxriffkml@users.noreply.github.com>
2024-03-21 12:49:30 +0800

    Added responsive footer

---
+++ src/lib/Components/Footer/Footer.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+        import * as footer_svg from "$lib/Components/SVG/footer_svg";
+
+        let footer_links = [
+            {name:'bsc scan', href:'https://bscscan.com/token/0xcF6BB5389c92Bdda8a3747Ddb454cB7a64626C63', icon: footer_svg.bsc_scan_svg},
+            {name:'discord', href:'https://discord.com/invite/pTQ9EBHYtF', icon: footer_svg.discord_svg},
+            {name:'telegram', href:'https://t.me/venusprotocol', icon: footer_svg.telegram_svg},
+            {name:'twitter', href:'https://twitter.com/VenusProtocol', icon: footer_svg.twitter_svg},
+            {name:'medium', href:'https://medium.com/@Venus_protocol', icon: footer_svg.medium_svg},
+            {name:'github', href:'https://github.com/VenusProtocol/', icon: footer_svg.github_svg}
+        ]
+</script>
+
+<div class="flex flex-col sm:flex-row items-start sm:items-center justify-end gap-6 p-4 h-auto sm:h-[56px] bg-transparent w-full">
+    <a href="https://docs-v4.venus.io/" target="_blank" class="underline text-sm">Documentation</a>
+
+    <div class="bg-[#303e5e] h-full w-[1px] hidden sm:block"></div>
+
+    <div class="flex flex-none items-center -ml-4 sm:ml-0">
+        {#each footer_links as footer}
+            <a class="bg-[#303e5e] hover:bg-[#3a78ff] text-white ml-4 flex h-6 w-6 items-center justify-center rounded" href={footer.href} target="_blank" rel="noreferrer">
+                {@html footer.icon}
+            </a>
+        {/each}
+    </div>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,19 +2,26 @@
 	import '../app.postcss';
 	import NavBar from '$lib/Components/NavBar/NavBar.svelte';
 	import SideBar from '$lib/Components/SideBar/SideBar.svelte';
+	import Footer from '$lib/Components/Footer/Footer.svelte'
 
 	// Floating UI for Popups
 	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
 	import { AppShell, storePopup } from '@skeletonlabs/skeleton';
 	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+
+	// Original classes for the Navbar, slot and Footer: <div class="w-full pl-0 md:pl-[5.2rem] xl:pl-[14rem]">
 </script>
 
 <div class="flex flex-row">
 	<div class="md:w-[5.2rem] xl:w-[14rem] hidden md:block fixed h-screen">
 		<SideBar />
 	</div>
-	<div class="w-full pl-0 md:pl-[5.2rem] xl:pl-[14rem]">
+	<div class="flex flex-col min-h-screen w-full pl-0 md:pl-[5.2rem] xl:pl-[14rem]">
 		<NavBar />
-		<slot />
-	</div>
+		<div class="flex-grow">
+			<slot />
+		</div>
+		<Footer />
+	</div>	
 </div>
+
